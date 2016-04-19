b2718d2cc9a70eda04822b7c8e11aa3e5fabface
Mathieu Lirzin <mthl@gnu.org>
2016-04-19 22:21:53 +0200

    maint: Define directory-local variables for Emacs.
    This configures Emacs automatically.
    
    * .dir-locals.el: New file.

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;; Per-directory local variables for GNU Emacs 23 and later.
+
+((nil . ((fill-column . 78)
+	 (tab-width   .  8)))
+ (c-mode . ((c-file-style . "gnu")
+	    (indent-tabs-mode . nil)))
+ (scheme-mode . ((indent-tabs-mode . nil))))
