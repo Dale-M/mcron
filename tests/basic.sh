c285d36ab2a48f09c14655a0dcec5895700fe4dd
Mathieu Lirzin <mthl@gnu.org>
2017-09-28 17:59:40 +0200

    tests: Add 'basic.sh'
    * tests/init.sh: New test framework from Gnulib.
    * tests/basic.sh: New test.
    * Makefile.am (TESTS): Add it.
    (TEST_EXTENSIONS): Add '.sh'.
    (SH_LOG_COMPILER): Use 'pre-inst-env'.
    (EXTRA_DIST): Add 'tests/init.sh'.
    * build-aux/pre-inst-env.in: export $srcdir for shell tests.

---
+++ tests/basic.sh
@@ -0,0 +1,33 @@
+# basic.sh -- basic tests for mcron
+# Copyright © 2017 Mathieu Lirzin <mthl@gnu.org>
+#
+# This file is part of GNU Mcron.
+#
+# GNU Mcron is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GNU Mcron is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GNU Mcron.  If not, see <http://www.gnu.org/licenses/>.
+
+source "${srcdir}/tests/init.sh"
+
+# Use current working directory to store mcron files
+XDG_CONFIG_HOME=`pwd`
+export XDG_CONFIG_HOME
+
+mkdir cron
+cat > cron/foo.guile <<EOF
+(job '(next-second) '(display "foo\n"))
+EOF
+
+mcron --schedule=1 > "output$$"
+grep -e "foo" "output$$" || fail_ "'foo.guile' job is not scheduled"
+
+Exit 0
